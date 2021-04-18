63df0dbc1cf3959afa5bfa6e456a08a843cc948d
charan <durgicharan81@gmail.com>
2021-04-18 15:08:02 +0530

    added default suggested user to start chat

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import './index.css';
 import App from './App.container';
 import reportWebVitals from './reportWebVitals';
 
-// if (process.env.NODE_ENV === 'production') {
-//   console.log = () => {};
-// }
+if (process.env.NODE_ENV === 'production') {
+  console.log = () => {};
+}
 ReactDOM.render(
   <Provider store={store}>
     <SocketProvider>

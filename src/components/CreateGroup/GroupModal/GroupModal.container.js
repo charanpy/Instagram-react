9e14b8042f5d131301764f50a8214d8fdeab6940
charan <durgicharan81@gmail.com>
2021-04-18 14:33:46 +0530

    prod test'

--- src/components/CreateGroup/GroupModal/GroupModal.container.js
+++ src/components/CreateGroup/GroupModal/GroupModal.container.js
@@ -28,10 +28,11 @@ const GroupModalContainer = ({
   const { input, loading } = searchUser;
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_API}profile/`).then((res) =>
-      setSearchUser((user) => ({
-        ...user,
-        defaultUsers: res.data.profiles,
-      }))
+      // setSearchUser((user) => ({
+      //   ...user,
+      //   defaultUsers: res.data.profiles,
+      // }))
+      console.log(res)
     );
   }, []);
   useEffect(() => {
